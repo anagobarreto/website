9f4fb0440a074a219e0275944455cb1eee4accce
Anacgbarreto <anacgbarreto@gmail.com>
2017-07-09 19:36:47 +0100

    Nav background scroll

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function() {
   $(window).on("scroll", function() {
     if($(window).scrollTop() > 110) {
-      $(".directory").addClass("active");
+      $("ul.nav").addClass("active");
     } else {
-      $(".directory").removeClass("active");
+      $("ul.nav").removeClass("active");
     }
   });
 });
