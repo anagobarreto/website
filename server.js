5872c0fee1db5604523468fc5ab3ab7b45f1a275
Anacgbarreto <anacgbarreto@gmail.com>
2017-07-14 16:00:25 +0100

    Fixed routing for projects

--- server.js
+++ server.js
@@ -23,8 +23,25 @@ app.get('/projects/alphabook', function (req, res) {
   res.render('projects/alphabook', { title: 'Alphabook' });
 });
 
+app.get('/projects/paws', function (req, res) {
+  res.render('projects/paws', { title: 'Paws & Play' });
+});
 
+app.get('/projects/novanoid', function (req, res) {
+  res.render('projects/novanoid', { title: 'Novanoid' });
+});
 
+app.get('/projects/orion', function (req, res) {
+  res.render('projects/orion', { title: 'Orions Dungeon' });
+});
+
+app.get('/projects/daft', function (req, res) {
+  res.render('projects/daft', { title: 'Daft Punk Soundboard' });
+});
+
+app.get('/projects/bikes', function (req, res) {
+  res.render('projects/bikes', { title: 'Santander cycles map' });
+});
 
 app.use(express.static('public'));
 
