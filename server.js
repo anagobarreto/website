8109fb55ff503e8c4a6d9b76b969dbe25527a38e
Anacgbarreto <anacgbarreto@gmail.com>
2019-02-24 14:59:46 -0800

    Updating port

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-app.get('/work', function (req, res) {
-  res.render('work', { title: 'Work' });
+app.get("/work", function(req, res) {
+  res.render("work", { title: "Work" });
 });
 
-app.get('/contact', function (req, res) {
-  res.render('contact', { title: 'Contact' });
+app.get("/contact", function(req, res) {
+  res.render("contact", { title: "Contact" });
 });
 
-app.get('/about', function (req, res) {
-  res.render('about', { title: 'About' });
+app.get("/about", function(req, res) {
+  res.render("about", { title: "About" });
 });
 
-app.get('/projects/alphabook', function (req, res) {
-  res.render('projects/alphabook', { title: 'Alphabook' });
+app.get("/projects/alphabook", function(req, res) {
+  res.render("projects/alphabook", { title: "Alphabook" });
 });
 
-app.get('/projects/paws', function (req, res) {
-  res.render('projects/paws', { title: 'Paws & Play' });
+app.get("/projects/paws", function(req, res) {
+  res.render("projects/paws", { title: "Paws & Play" });
 });
 
-app.get('/projects/novanoid', function (req, res) {
-  res.render('projects/novanoid', { title: 'Novanoid' });
+app.get("/projects/novanoid", function(req, res) {
+  res.render("projects/novanoid", { title: "Novanoid" });
 });
 
-app.get('/projects/orion', function (req, res) {
-  res.render('projects/orion', { title: 'Orions Dungeon' });
+app.get("/projects/orion", function(req, res) {
+  res.render("projects/orion", { title: "Orions Dungeon" });
 });
 
-app.get('/projects/daft', function (req, res) {
-  res.render('projects/daft', { title: 'Daft Punk Soundboard' });
+app.get("/projects/daft", function(req, res) {
+  res.render("projects/daft", { title: "Daft Punk Soundboard" });
 });
 
-app.get('/projects/bikes', function (req, res) {
-  res.render('projects/bikes', { title: 'Santander cycles map' });
+app.get("/projects/bikes", function(req, res) {
+  res.render("projects/bikes", { title: "Santander cycles map" });
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
